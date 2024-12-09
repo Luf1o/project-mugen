17ac791d6f270d8a3b899ecc8f0c2d67acad945c
Jyothish <117960824+Luf1o@users.noreply.github.com>
2024-12-09 12:44:52 +0530

    Implemented Auth

--- mugen/app.py
+++ mugen/app.py
@@ -1,17 +1,91 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'super-key-101'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 @app.route('/')
+def index():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    return render_template('index.html')
+
+@app.route('/home')
+@login_required
 def home():
-    return render_template("index.html")
+    return render_template('home.html')
 
-@app.route("/login")
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.password_hash == password:
+            login_user(user)
+            flash('Login successful', 'success')
+            return redirect(url_for('home'))
+        flash('Invalid username or password', 'danger')
+    return render_template('login.html')
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password1']
+        if User.query.filter_by(username=username).first():
+            flash('User already exists', 'danger')
+        else:
+            new_user = User(username=username, password_hash=password)
+            db.session.add(new_user)
+            db.session.commit()
+            flash('User registered successfully', 'success')
+            return redirect(url_for('login'))  # Change 'redirectLogin' to 'login'
+    return render_template('register.html')
 
-@app.route("/register")
-def registerAccount():
-    return render_template("register.html")
-app.run()
+@app.route('/logout', methods=['POST'])
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out', 'success')
+    return redirect(url_for('login'))
+
+@app.route('/d-user/<username>', methods=['POST'])
+@login_required
+def delete_user(username):
+    user = User.query.filter_by(username=username).first()
+    if user:
+        db.session.delete(user)
+        db.session.commit()
+        flash('User deleted successfully', 'success')
+    else:
+        flash('User not found', 'danger')
+    return redirect(url_for('home'))
+
+@app.route('/chat')
+@login_required
+def chat():
+    return render_template('chat.html')
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
